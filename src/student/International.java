7f820ae8f7d68601f01edb90401396a654c1ad22
HyeonOh678 <bryanhasu@gmail.com>
2023-02-27 17:55:14 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/CS213_Tuition_Project/student/EnrollStudent.class
    #	out/production/CS213_Tuition_Project/student/Enrollment.class
    #	out/production/CS213_Tuition_Project/student/NonResident.class
    #	out/production/CS213_Tuition_Project/student/Resident.class
    #	out/production/CS213_Tuition_Project/student/Roster.class
    #	out/production/CS213_Tuition_Project/student/Student.class
    #	out/production/CS213_Tuition_Project/student/TuitionManager.class

--- src/student/International.java
+++ src/student/International.java
@@ -1,5 +1,11 @@
 package student;
 
+/**
+ * Class to implement International subclass which extends to its parent class NonResident
+ * Contains instance variable isStudyAbroad which will be used in tuition calculation purposes
+ * Contains overrided methods such as toString, isValid, and getType
+ * @author Nikhil Agarwal, Hyeon Oh
+ */
 public class International extends NonResident{
 
     public boolean isStudyAbroad;
@@ -7,16 +13,34 @@ public class International extends NonResident{
     public static final int MAX_STUDY_ABROAD_CREDITS = 12;
     public static final int MIN_INTERNATIONAL_CREDITS_ENROLLED = 12;
 
+    /**
+     * Constructor of the International subclass
+     * This constructor will have isStudyAbroad set to false as default
+     * @param profile of the international student
+     * @param major major of the international student
+     * @param creditCompleted creditCompleted of the international student
+     */
     public International(Profile profile,Major major, int creditCompleted){
         super(profile,major,creditCompleted);
         this.isStudyAbroad = false;
     }
 
+    /**
+     * Constructor for the International subclass
+     * @param profile profile of the international student
+     * @param major major of the international student
+     * @param creditCompleted creditCompleted of the international student
+     * @param isStudyAbroad value of whether or not the student is studying abroad
+     */
     public International(Profile profile, Major major, int creditCompleted, boolean isStudyAbroad){
         super(profile,major,creditCompleted);
         this.isStudyAbroad = isStudyAbroad;
     }
 
+    /**
+     * Overrides the toString method
+     * @return invokes the parent class toString and shows whether or not the student is studying abroad
+     */
     @Override
     public String toString(){
         if(isStudyAbroad == false){
@@ -26,6 +50,10 @@ public class International extends NonResident{
         }
     }
 
+    /**
+     * Gets the type of international student
+     * @return if the student is studying abroad, returns a String that tells that the international student is studying abroad
+     */
     @Override
     public String getType(){
         if(isStudyAbroad){
@@ -34,6 +62,11 @@ public class International extends NonResident{
         return "(International student)";
     }
 
+    /**
+     * method which checks whether or not the student's credit fall within the range of an international student that is abroad
+     * @param creditsEnrolled creditsEnrolle of the student
+     * @return true if the number of creditsEnrolled are valid, false otherwise
+     */
     @Override
     public boolean isValid(int creditsEnrolled){
         if(isStudyAbroad){
