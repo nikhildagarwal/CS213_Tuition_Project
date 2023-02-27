c2726ed4025ed3a4055340b1406660fc6bec308f
HyeonOh678 <bryanhasu@gmail.com>
2023-02-27 17:59:13 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/CS213_Tuition_Project/student/EnrollStudent.class
    #	out/production/CS213_Tuition_Project/student/Enrollment.class
    #	out/production/CS213_Tuition_Project/student/NonResident.class
    #	out/production/CS213_Tuition_Project/student/Resident.class
    #	out/production/CS213_Tuition_Project/student/Roster.class
    #	out/production/CS213_Tuition_Project/student/Student.class
    #	out/production/CS213_Tuition_Project/student/TuitionManager.class

--- src/student/TriState.java
+++ src/student/TriState.java
@@ -1,14 +1,31 @@
 package student;
 
+/**
+ * Implementation of subclass TriState which extends to parent class NonResident
+ * Contains instance variable state
+ * Contains overrided methods such as toString
+ * @author Nikhil Agarwal, Hyeon Oh
+ */
 public class TriState extends NonResident{
 
     private String state;
 
+    /**
+     * Constructor for the TriState subclass object
+     * @param profile profile of the student
+     * @param major major of the student
+     * @param creditCompleted creditCompleted of the student
+     * @param state state of the student
+     */
     public TriState(Profile profile, Major major, int creditCompleted, String state){
         super(profile,major,creditCompleted);
         this.state = state;
     }
 
+    /**
+     * Overrides the toString method
+     * @return invokes the parent class toString method and adds the information that the student is from a tri-state
+     */
     @Override
     public String toString(){
         return super.toString()+"(tri-state:"+state+")";
