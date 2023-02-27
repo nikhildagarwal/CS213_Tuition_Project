0037fbf4d631d74ac511e1aa002e36d561b5a26e
HyeonOh678 <bryanhasu@gmail.com>
2023-02-27 14:55:44 -0500

    resident comment test and rostertest update for remove

--- src/student/RosterTest.java
+++ src/student/RosterTest.java
@@ -53,5 +53,51 @@ public class RosterTest {
 
         assertFalse(roster.add(res));
     }
+
+    @Test
+    public void removeInternationalStudentTrue() {
+        Date d = new Date();
+        Profile p = new Profile("Funcheon","Hope",d);
+        International i = new International(p,Major.MATH,101,true);
+        Roster roster = new Roster();
+        roster.add(i);
+
+        assertTrue(roster.remove(i));
+    }
+
+    @Test
+    public void removeInternationalStudentFalse()
+    {
+        Date d = new Date();
+        Profile p = new Profile("Funcheon","Hope",d);
+        International i = new International(p,Major.MATH,101,true);
+        Roster roster = new Roster();
+        //this time the student is not added so the student cannot be removed
+
+        assertFalse(roster.remove(i));
+    }
+
+    @Test
+    public void removeTriStateTrue()
+    {
+        Date d = new Date();
+        Profile p = new Profile("Funcheon", "Hope", d);
+        TriState res = new TriState(p, Major.CS, 132, "NJ");
+        Roster roster = new Roster();
+        //student is added
+        roster.add(res);
+        assertTrue(roster.remove(res));
+    }
+
+    @Test
+    public void removeTriStateFalse()
+    {
+        Date d = new Date();
+        Profile p = new Profile("Funcheon", "Hope", d);
+        TriState res = new TriState(p, Major.CS, 132, "NJ");
+        Roster roster = new Roster();
+        //student is not added to the roster so cannot be removed.
+        assertFalse(roster.remove(res));
+    }
 }
 
